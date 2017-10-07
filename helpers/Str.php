449a7eb16491e345ff1d350aaf9e0ce873dfa1d6
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2017-10-07 21:17:28 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- helpers/Str.php
+++ helpers/Str.php
@@ -2,262 +2,262 @@
 
 class Str extends Prefab
 {
-    protected static $snakeCache = [];
-    protected static $camelCache = [];
-    protected static $studlyCache = [];
+	protected static $snakeCache = [];
+	protected static $camelCache = [];
+	protected static $studlyCache = [];
     
-    public static function after($subject, $search)
-    {
-        return $search === '' ? $subject : array_reverse(explode($search, $subject, 2))[0];
-    }
+	public static function after($subject, $search)
+	{
+		return $search === '' ? $subject : array_reverse(explode($search, $subject, 2))[0];
+	}
 
-    public static function before($subject, $search)
-    {
-        return $search === '' ? $subject : explode($search, $subject)[0];
-    }
+	public static function before($subject, $search)
+	{
+		return $search === '' ? $subject : explode($search, $subject)[0];
+	}
 
-    public static function camel($value)
-    {
-        if (isset(static::$camelCache[$value])) {
-            return static::$camelCache[$value];
-        }
+	public static function camel($value)
+	{
+		if (isset(static::$camelCache[$value])) {
+			return static::$camelCache[$value];
+		}
 
-        return static::$camelCache[$value] = lcfirst(static::studly($value));
-    }
+		return static::$camelCache[$value] = lcfirst(static::studly($value));
+	}
 
-    public static function contains($haystack, $needles)
-    {
-        foreach ((array) $needles as $needle) {
-            if ($needle !== '' && mb_strpos($haystack, $needle) !== false) {
-                return true;
-            }
-        }
+	public static function contains($haystack, $needles)
+	{
+		foreach ((array) $needles as $needle) {
+			if ($needle !== '' && mb_strpos($haystack, $needle) !== false) {
+				return true;
+			}
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    public static function endsWith($haystack, $needles)
-    {
-        foreach ((array) $needles as $needle) {
-            if (substr($haystack, -strlen($needle)) === (string) $needle) {
-                return true;
-            }
-        }
+	public static function endsWith($haystack, $needles)
+	{
+		foreach ((array) $needles as $needle) {
+			if (substr($haystack, -strlen($needle)) === (string) $needle) {
+				return true;
+			}
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    public static function finish($value, $cap)
-    {
-        $quoted = preg_quote($cap, '/');
+	public static function finish($value, $cap)
+	{
+		$quoted = preg_quote($cap, '/');
 
-        return preg_replace('/(?:'.$quoted.')+$/u', '', $value).$cap;
-    }
+		return preg_replace('/(?:'.$quoted.')+$/u', '', $value).$cap;
+	}
 
-    public static function is($pattern, $value)
-    {
-        $patterns = is_array($pattern) ? $pattern : (array) $pattern;
+	public static function is($pattern, $value)
+	{
+		$patterns = is_array($pattern) ? $pattern : (array) $pattern;
 
-        if (empty($patterns)) {
-            return false;
-        }
+		if (empty($patterns)) {
+			return false;
+		}
 
-        foreach ($patterns as $pattern) {
-            if ($pattern == $value) {
-                return true;
-            }
+		foreach ($patterns as $pattern) {
+			if ($pattern == $value) {
+				return true;
+			}
 
-            $pattern = preg_quote($pattern, '#');
-            $pattern = str_replace('\*', '.*', $pattern);
+			$pattern = preg_quote($pattern, '#');
+			$pattern = str_replace('\*', '.*', $pattern);
 
-            if (preg_match('#^'.$pattern.'\z#u', $value) === 1) {
-                return true;
-            }
-        }
+			if (preg_match('#^'.$pattern.'\z#u', $value) === 1) {
+				return true;
+			}
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    public static function kebab($value)
-    {
-        return static::snake($value, '-');
-    }
+	public static function kebab($value)
+	{
+		return static::snake($value, '-');
+	}
 
-    public static function length($value, $encoding = null)
-    {
-        if ($encoding) {
-            return mb_strlen($value, $encoding);
-        }
+	public static function length($value, $encoding = null)
+	{
+		if ($encoding) {
+			return mb_strlen($value, $encoding);
+		}
 
-        return mb_strlen($value);
-    }
+		return mb_strlen($value);
+	}
 
-    public static function limit($value, $limit = 100, $end = '...')
-    {
-        if (mb_strwidth($value, 'UTF-8') <= $limit) {
-            return $value;
-        }
+	public static function limit($value, $limit = 100, $end = '...')
+	{
+		if (mb_strwidth($value, 'UTF-8') <= $limit) {
+			return $value;
+		}
 
-        return rtrim(mb_strimwidth($value, 0, $limit, '', 'UTF-8')).$end;
-    }
+		return rtrim(mb_strimwidth($value, 0, $limit, '', 'UTF-8')).$end;
+	}
 
-    public static function lower($value)
-    {
-        return mb_strtolower($value, 'UTF-8');
-    }
+	public static function lower($value)
+	{
+		return mb_strtolower($value, 'UTF-8');
+	}
 
-    public static function words($value, $words = 100, $end = '...')
-    {
-        preg_match('/^\s*+(?:\S++\s*+){1,'.$words.'}/u', $value, $matches);
+	public static function words($value, $words = 100, $end = '...')
+	{
+		preg_match('/^\s*+(?:\S++\s*+){1,'.$words.'}/u', $value, $matches);
 
-        if (!isset($matches[0]) || static::length($value) === static::length($matches[0])) {
-            return $value;
-        }
+		if (!isset($matches[0]) || static::length($value) === static::length($matches[0])) {
+			return $value;
+		}
 
-        return rtrim($matches[0]).$end;
-    }
+		return rtrim($matches[0]).$end;
+	}
 
-    public static function parseCallback($callback, $default = null)
-    {
-        return static::contains($callback, '@') ? explode('@', $callback, 2) : [$callback, $default];
-    }
+	public static function parseCallback($callback, $default = null)
+	{
+		return static::contains($callback, '@') ? explode('@', $callback, 2) : [$callback, $default];
+	}
 
-    public static function plural($value, $count = 2)
-    {
-        return Pluralizer::plural($value, $count);
-    }
+	public static function plural($value, $count = 2)
+	{
+		return Pluralizer::plural($value, $count);
+	}
 
-    public static function random($length = 16)
-    {
-        $string = '';
+	public static function random($length = 16)
+	{
+		$string = '';
 
-        while (($len = strlen($string)) < $length) {
-            $size = $length - $len;
-            $bytes = random_bytes($size);
-            $string .= substr(str_replace(['/', '+', '='], '', base64_encode($bytes)), 0, $size);
-        }
+		while (($len = strlen($string)) < $length) {
+			$size = $length - $len;
+			$bytes = random_bytes($size);
+			$string .= substr(str_replace(['/', '+', '='], '', base64_encode($bytes)), 0, $size);
+		}
 
-        return $string;
-    }
+		return $string;
+	}
 
-    public static function replaceArray($search, array $replace, $subject)
-    {
-        foreach ($replace as $value) {
-            $subject = static::replaceFirst($search, $value, $subject);
-        }
+	public static function replaceArray($search, array $replace, $subject)
+	{
+		foreach ($replace as $value) {
+			$subject = static::replaceFirst($search, $value, $subject);
+		}
 
-        return $subject;
-    }
+		return $subject;
+	}
 
-    public static function replaceFirst($search, $replace, $subject)
-    {
-        if ($search == '') {
-            return $subject;
-        }
+	public static function replaceFirst($search, $replace, $subject)
+	{
+		if ($search == '') {
+			return $subject;
+		}
 
-        $position = strpos($subject, $search);
+		$position = strpos($subject, $search);
 
-        if ($position !== false) {
-            return substr_replace($subject, $replace, $position, strlen($search));
-        }
+		if ($position !== false) {
+			return substr_replace($subject, $replace, $position, strlen($search));
+		}
 
-        return $subject;
-    }
+		return $subject;
+	}
 
-    public static function replaceLast($search, $replace, $subject)
-    {
-        $position = strrpos($subject, $search);
+	public static function replaceLast($search, $replace, $subject)
+	{
+		$position = strrpos($subject, $search);
 
-        if ($position !== false) {
-            return substr_replace($subject, $replace, $position, strlen($search));
-        }
+		if ($position !== false) {
+			return substr_replace($subject, $replace, $position, strlen($search));
+		}
 
-        return $subject;
-    }
+		return $subject;
+	}
 
-    public static function start($value, $prefix)
-    {
-        $quoted = preg_quote($prefix, '/');
-        return $prefix.preg_replace('/^(?:'.$quoted.')+/u', '', $value);
-    }
+	public static function start($value, $prefix)
+	{
+		$quoted = preg_quote($prefix, '/');
+		return $prefix.preg_replace('/^(?:'.$quoted.')+/u', '', $value);
+	}
 
-    public static function upper($value)
-    {
-        return mb_strtoupper($value, 'UTF-8');
-    }
+	public static function upper($value)
+	{
+		return mb_strtoupper($value, 'UTF-8');
+	}
 
-    public static function title($value)
-    {
-        return mb_convert_case($value, MB_CASE_TITLE, 'UTF-8');
-    }
+	public static function title($value)
+	{
+		return mb_convert_case($value, MB_CASE_TITLE, 'UTF-8');
+	}
 
-    public static function singular($value)
-    {
-        return Pluralizer::singular($value);
-    }
+	public static function singular($value)
+	{
+		return Pluralizer::singular($value);
+	}
 
-    public static function slug($title, $separator = '-', $language = 'en')
-    {
-        $title = static::ascii($title, $language);
+	public static function slug($title, $separator = '-', $language = 'en')
+	{
+		$title = static::ascii($title, $language);
 
-        $flip = $separator == '-' ? '_' : '-';
+		$flip = $separator == '-' ? '_' : '-';
 
-        $title = preg_replace('!['.preg_quote($flip).']+!u', $separator, $title);
-        $title = str_replace('@', $separator.'at'.$separator, $title);
-        $title = preg_replace('![^'.preg_quote($separator).'\pL\pN\s]+!u', '', mb_strtolower($title));
-        $title = preg_replace('!['.preg_quote($separator).'\s]+!u', $separator, $title);
+		$title = preg_replace('!['.preg_quote($flip).']+!u', $separator, $title);
+		$title = str_replace('@', $separator.'at'.$separator, $title);
+		$title = preg_replace('![^'.preg_quote($separator).'\pL\pN\s]+!u', '', mb_strtolower($title));
+		$title = preg_replace('!['.preg_quote($separator).'\s]+!u', $separator, $title);
 
-        return trim($title, $separator);
-    }
+		return trim($title, $separator);
+	}
 
-    public static function snake($value, $delimiter = '_')
-    {
-        $key = $value;
+	public static function snake($value, $delimiter = '_')
+	{
+		$key = $value;
 
-        if (isset(static::$snakeCache[$key][$delimiter])) {
-            return static::$snakeCache[$key][$delimiter];
-        }
+		if (isset(static::$snakeCache[$key][$delimiter])) {
+			return static::$snakeCache[$key][$delimiter];
+		}
 
-        if (!ctype_lower($value)) {
-            $value = preg_replace('/\s+/u', '', ucwords($value));
+		if (!ctype_lower($value)) {
+			$value = preg_replace('/\s+/u', '', ucwords($value));
 
-            $value = static::lower(preg_replace('/(.)(?=[A-Z])/u', '$1'.$delimiter, $value));
-        }
+			$value = static::lower(preg_replace('/(.)(?=[A-Z])/u', '$1'.$delimiter, $value));
+		}
 
-        return static::$snakeCache[$key][$delimiter] = $value;
-    }
+		return static::$snakeCache[$key][$delimiter] = $value;
+	}
 
-    public static function startsWith($haystack, $needles)
-    {
-        foreach ((array) $needles as $needle) {
-            if ($needle !== '' && substr($haystack, 0, strlen($needle)) === (string) $needle) {
-                return true;
-            }
-        }
+	public static function startsWith($haystack, $needles)
+	{
+		foreach ((array) $needles as $needle) {
+			if ($needle !== '' && substr($haystack, 0, strlen($needle)) === (string) $needle) {
+				return true;
+			}
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    public static function studly($value)
-    {
-        $key = $value;
+	public static function studly($value)
+	{
+		$key = $value;
 
-        if (isset(static::$studlyCache[$key])) {
-            return static::$studlyCache[$key];
-        }
+		if (isset(static::$studlyCache[$key])) {
+			return static::$studlyCache[$key];
+		}
 
-        $value = ucwords(str_replace(['-', '_'], ' ', $value));
+		$value = ucwords(str_replace(['-', '_'], ' ', $value));
 
-        return static::$studlyCache[$key] = str_replace(' ', '', $value);
-    }
+		return static::$studlyCache[$key] = str_replace(' ', '', $value);
+	}
 
-    public static function substr($string, $start, $length = null)
-    {
-        return mb_substr($string, $start, $length, 'UTF-8');
-    }
+	public static function substr($string, $start, $length = null)
+	{
+		return mb_substr($string, $start, $length, 'UTF-8');
+	}
 
-    public static function ucfirst($string)
-    {
-        return static::upper(static::substr($string, 0, 1)).static::substr($string, 1);
-    }
+	public static function ucfirst($string)
+	{
+		return static::upper(static::substr($string, 0, 1)).static::substr($string, 1);
+	}
 }
